a6945ee579ee5d86274f97b59873f4294e39e6b3
Alexis Davila <adavila1999z@gmail.com>
2024-11-13 23:16:43 -0300

    Agregadas vistas y panltilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class ImageUpload(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __srt_(self):
+        return self.title
